ee63cb9824b8ac42634113dc8842367e32cb456f
niicodeer <nicoradin.job@gmail.com>
2023-11-23 22:05:54 -0300

    added models and migrations

---
+++ app/Models/Employee.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'first_name',
+        'last_name',
+        'address',
+        'city_id',
+        'state_id',
+        'country_id',
+        'department_id',
+        'zip_code',
+        'birth_date',
+        'date_hired'
+        ];
+
+
+        public function country(): BelongsTo
+        {
+            return $this->belongsTo(Country::class);
+        }
+
+        public function state(): BelongsTo
+        {
+            return $this->belongsTo(State::class);
+        }
+
+        public function city(): BelongsTo
+        {
+            return $this->belongsTo(City::class);
+        }
+
+        public function department(): BelongsTo
+        {
+            return $this->belongsTo(Department::class);
+        }
+
+}
